10a398fb8d7efe1ab01cb0b6b1d08cb60d380dd2
Ayush06102000 <akmeansayushkumar@gmail.com>
2024-10-05 23:56:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { userRouter } = require("./routes/user")
+const { productRouter } = require("./routes/product");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/product",productRouter);
+
+app.listen(port,()=>{
+    console.log(`listening to ${port}`)
+})

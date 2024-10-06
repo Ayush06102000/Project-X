0e857374230f5205e3e99a264b90bb707ee7fe53
Ayush06102000 <akmeansayushkumar@gmail.com>
2024-10-06 14:39:57 +0530

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const express = require('express');
+var cors = require('cors')
 const jwt = require('jsonwebtoken');
 const { userRouter } = require("./routes/user")
 const { productRouter } = require("./routes/product");
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json())
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/product",productRouter);
